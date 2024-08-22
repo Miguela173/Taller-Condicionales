a9df8f59d18c29e5cddc33de645253193b2c96b1
Miguel Angel <miguelpatval09@gmail.com>
2024-08-21 22:04:15 -0500

    parte1

---
+++ script.js
@@ -0,0 +1,8 @@
+/* 1-Ejercicio  */
+
+let edad = parseInt(prompt("Ingrese su edad: "));
+if (edad >= 18) {
+    console.log("Eres mayor de edad");
+} else {
+    console.log("Eres menor de edad");
+}
