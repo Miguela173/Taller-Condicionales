880b0b5ec3ca069d22544b03b1e541b7106d554c
Miguel Angel <miguelpatval09@gmail.com>
2024-08-21 22:39:02 -0500

    parte15

--- script.js
+++ script.js
@@ -161,3 +161,19 @@ if (hora >= 6 && hora < 12) {
 } else {
     console.log("Hora no válida");
 }
+
+/* 15-Ejercicio */
+
+let peso = parseFloat(prompt("Ingrese su peso en kilogramos: "));
+let altura = parseFloat(prompt("Ingrese su altura en metros: "));
+let imc = peso / (altura * altura);
+if (imc < 18.5) {
+    console.log("Bajo peso");
+} else if (imc >= 18.5 && imc <= 24.9) {
+    console.log("Peso normal");
+} else if (imc >= 25 && imc <= 29.9) {
+    console.log("Sobrepeso");
+} else if (imc > 30) {
+    console.log("Obesidad");
+} 
+
